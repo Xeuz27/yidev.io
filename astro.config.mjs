9ccd4d6d95e44c6a60f1bb7eb287ef2253703495
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-09-13 20:46:20 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://xeuz27.github.io',
+    base: '/yidev.io',
     integrations: [tailwind()],
 });
