ea37adebe810b32f6cbaac570d0297cda6caa07e
Xeuz27 <jesusg.1301.jg@gmail.com>
2025-01-22 06:48:28 -0400

    test?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://xeuz27.github.io',
-    base: '/yidev.io',
+    base: '',
     integrations: [tailwind()],
 });
